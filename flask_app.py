ef635caac7084537baa4e4da9c88f7fe29ae0c6d
Dániel Ernő Szabó <daniel.erno.szabo@LEGO.com>
2018-07-24 14:06:21 +0200

    Fixed bug in source, the short reason is that you need to use dotenv module to source variables from different files, because the .bashrc is ignored when the process running your app is spinning up!

--- flask_app.py
+++ flask_app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template,g
-from github import Github 
-import json
+from flask import Flask, render_template
+from github import Github
 import requests
+import sys
 import os
 from pprint import pprint
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/mysite')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 app = Flask(__name__)
 
+TOKEN = os.getenv('PAT')
+CHNID = os.getenv('YTCHN')
+CHAPI = os.getenv('YTAPI')
+	
+gth = Github(TOKEN)
+
 @app.route("/")
 def index():
-	TOKEN = os.getenv('PAT')
-	CHNID = os.getenv('YTCHN')
-	CHAPI = os.getenv('YTAPI')
-
-	gth = Github(TOKEN)
-	Channel = json.loads(requests.get(url="https://www.googleapis.com/youtube/v3/channels?part=snippet&id={}&key={}".format(CHNID,CHAPI)).text)['items'][0]['snippet']
-	Playlists = json.loads(requests.get(url="https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId={}&key={}&maxResults=50".format(CHNID,CHAPI)).text)['items']
+	Channel = requests.get(url="https://www.googleapis.com/youtube/v3/channels?part=snippet&id={}&key={}".format(CHNID,CHAPI)).json()['items'][0]['snippet']
+	Playlists = requests.get(url="https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId={}&key={}&maxResults=50".format(CHNID,CHAPI)).json()['items']
 	ChannelInfo = {
 		'ID': CHNID,
 		'Title': Channel['title'],
@@ -24,30 +29,22 @@ def index():
 	}
 
 
-	for item in Playlists:		
-		print('#################################################')
-		print('#################################################')
+	for item in Playlists:
 		ItemsToProcess = []
 
 		url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={}&key={}".format(item['id'],CHAPI)
 
-		PlaylistResponse = json.loads(requests.get(url=url).text)
-		print(len(PlaylistResponse['items']))
+		PlaylistResponse = requests.get(url=url).json()
 		ItemsToProcess.append(PlaylistResponse['items'])
 
-		print(item['id'],item['snippet']['title'])
 		while PlaylistResponse.get('nextPageToken'):
 			url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&pageToken={}&playlistId={}&key={}".format(PlaylistResponse.get('nextPageToken'),item['id'],CHAPI)
-			PlaylistResponse = json.loads(requests.get(url=url).text)
-			print(len(PlaylistResponse['items']))
+			PlaylistResponse = requests.get(url=url).json()
 			ItemsToProcess.append(PlaylistResponse['items'])
 
-		
 		for kitem in ItemsToProcess:
-			print(len(kitem))
-			for jitem in kitem:	
+			for jitem in kitem:
 				ChannelInfo['Videos'].append({'ChannelName':item['snippet']['title'],'VideoName':jitem['snippet']['title'],'ID':jitem['snippet']['resourceId']['videoId'],'Description':jitem['snippet']['description'].replace('\n\n','<br>').replace('\n','<br>')})
-		print('#################################################')
-	#return render_template("index.html")
-	return render_template("index.html", repos = [ _ for _ in gth.get_user().get_repos() if not 'r3ap3rpy' in _.name], ytbchnl = ChannelInfo)
+
+	return render_template("index.html", repos =[ _ for _ in gth.get_user().get_repos() if not 'r3ap3rpy' in _.name], ytbchnl = ChannelInfo)
 
